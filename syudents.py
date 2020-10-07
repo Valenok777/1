e321522a2d3cb567c3b764c7ffd8b640868aeeb6
Valenok777 <71389430+Valenok777@users.noreply.github.com>
2020-10-07 17:45:25 +0300

    Add files via upload

---
+++ syudents.py
@@ -0,0 +1,8 @@
+person1 = {"name" : "Артем", "age" : 17, "ocupation" : "студент"}
+person2 = {"name" : "Герман", "age" : 15, "ocupation" : "студент"}
+person3 = {"name" : "Матвей", "age" : 14, "ocupation" : "студент"}
+person4 = {"name" : "Софья", "age" : 14, "ocupation" : "студент"}
+person5 = {"name" : "Дмитрий", "age" : 17, "ocupation" : "студент"}
+
+syudents = [person1, person2, person3, person4, person5]
+print(syudents[0].get("name"))
