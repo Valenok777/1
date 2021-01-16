45bb766ed4324450ea3cddfc432fa0d67c43d89c
Valenok777 <71389430+Valenok777@users.noreply.github.com>
2021-01-16 13:48:31 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import random
 from ursina import *
 from ursina.prefabs.platformer_controller_2d import PlatformerController2d
 
 
 # создаю экземпляры классов
 app = Ursina()
+
+window.fullscreen = True
 
 player = player_controller = PlatformerController2d(
 	position = (0, 0),
@@ -15,6 +18,7 @@ surface = Entity(
     model = 'quad',
     collider = 'box',
     scale=(50,1),
+    color = color.green,
 	)
 
 camera.add_script(SmoothFollow(target=player, offset=[0,5,-30], speed=40))
@@ -28,6 +32,16 @@ def input(key):
     if key == 'space':
         player.y += 1
         invoke(setattr, player, 'y', player.y-1, delay=.25)
+
+for i in range(11):
+    blok_1 = 0
+    for J in range(4):
+        blok = Entity(model = "quad",
+                      x = blok_1,
+                      y = random.randint(1, 8)
+                      )
+        blok_1 += 1
+
 
 
 # start running the game
