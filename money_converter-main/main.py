d1b6f1d45edb58b435a9c50b9deacc32aeeaa100
Valenok777 <71389430+Valenok777@users.noreply.github.com>
2021-05-26 17:46:27 +0300

    Add files via upload

--- money_converter-main/main.py
+++ money_converter-main/main.py
@@ -8,10 +8,21 @@ ui.setupUi(MainWindow)
 MainWindow.show()
 
 def test():
-	ruble = int(ui.ruble.toPlainText())
-	print(type(ruble))
-	WWW = ruble / 2
-	ui.dollar.setText(WWW)
+    ruble = int(ui.ruble.toPlainText())
+    dollar_1 = 73.54
+    dollar = ruble / dollar_1
+    ui.dollar.setText(str(round(dollar, 2)))
+    euro_1 = 89.94
+    euro = ruble / euro_1
+    ui.euro.setText(str(round(euro, 2)))
+    dinar_1 = 244.50
+    dinar = ruble / dinar_1
+    ui.dinar.setText(str(round(dinar, 2)))
+    rial_1 = 0.0017
+    rial = ruble / rial_1
+    ui.rial.setText(str(round(rial, 2)))
+
+
 
 
 ui.count.clicked.connect(test)
